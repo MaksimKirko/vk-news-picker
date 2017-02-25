38fbfded2e38ee112bda96bda8a555ea9a193b4c
MIKL99999 <radykevich@gmail.com>
2017-02-25 18:10:09 +0300

    sendFile

--- main.js
+++ main.js
@@ -6,27 +6,38 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-let parseUrl = url.parse(req.url);
-switch (parseUrl.pathname){
-case '/':
-fs.readFile('./index.html', function (err, html) {
-if (err) {
-throw err;
-}
-res.write(html);
-res.end();
-});
-break;
-case '/feed':
-res.end('FEED!');
-break;
-default:
-res.statusCode = 404;
-res.end();
-break;
-}
+
+    let parseUrl = url.parse(req.url);
+
+    switch (parseUrl.pathname) {
+        case '/':
+            let file = new fs.createReadStream('./www/index.html');
+            sendFile(file,res);
+            break;
+        case '/feed':
+            res.end('FEED!');
+            break;
+        default:
+            res.statusCode = 404;
+            res.end();
+            break;
+    }
 });
 
 server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function sendFile(file,res) {
+    file.pipe(res);
+
+    file.on('error', function (err) {
+        res.statusCode = 500;
+        res.end();
+        console.error(err);
+    });
+
+    res.on('close', function () {
+        file.destroy();
+    })
+}
