4ecae767d7703f8f2c11959902dedad951a11eb9
MIKL99999 <radykevich@gmail.com>
2017-03-04 19:14:28 +0300

    test db

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const pool = require('./Pool');
+const getAllUsers = require('./dao/User');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-
-    let parseUrl = url.parse(req.url);
-
-    switch (parseUrl.pathname) {
-        case '/':
-            let file = new fs.createReadStream('./www/index.html');
-            sendFile(file,res);
-            break;
-        case '/feed':
-            res.end('FEED!');
-            break;
-        default:
-            res.statusCode = 404;
-            res.end();
-            break;
-    }
-});
+        let parseUrl;
+        try {
+            parseUrl = url.parse(req.url);
+        } catch (err) {
+            console.error(err);
+        }
+        switch (parseUrl.pathname) {
+            case '/':
+                let file = new fs.createReadStream('./www/index.html');
+                sendFile(file, res);
+                break;
+            case '/users':
+                getAllUsers(pool, function (err, users) {
+                    for (let i = 0; i < users.length; i++)
+                        res.write("ID = " + users[i].id + "\nName = "+ users[i].name + "\n\n");
+                    res.end();
+                });
+                break;
+            default:
+                res.statusCode = 404;
+                res.end();
+                break;
+        }
+    })
+    ;
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-function sendFile(file,res) {
+function sendFile(file, res) {
     file.pipe(res);
 
     file.on('error', function (err) {
