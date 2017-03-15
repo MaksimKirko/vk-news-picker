45b51bfce12105c6426745e7eb6d930226954423
MaximKirko <kirko_ma_14@mf.grsu.by>
2017-03-15 17:59:52 +0300

    added getnewsPhotos function

--- public/vk-api/news-service.js
+++ public/vk-api/news-service.js
@@ -6,3 +6,36 @@ function getNews(tags, count, callback) {
         callback(data);
     });
 }
+
+function getNewsText(response) {
+    return response.text;
+}
+
+const sizeToSrc = {
+    75: 'src_small',
+    130: 'src',
+    604: 'src_big',
+    807: 'src_xbig',
+    1280: 'src_xxbig',
+    2560: 'src_xxxbig'
+};
+
+/**
+ * @description Get response[i] attachments photos. Size can be 75, 130, 604, 807, 1280, 2560
+ * @param {Object} response
+ * @param {Number} size
+ * @returns {Object} array of news photos
+ */
+function getNewsPhoto(response, size) {
+    var result = [];
+    for (var j = 0; j < response.attachments.length; j++) {
+        if (response.attachments[j].type == 'photo') {
+            result.push(mapSizeToSource(response.attachments[j], size, sizeToSrc));
+        }
+    }
+    return result;
+}
+
+function mapSizeToSource(attachment, size, sizeToSrc) {
+    return attachment.photo[sizeToSrc[size]];
+}
